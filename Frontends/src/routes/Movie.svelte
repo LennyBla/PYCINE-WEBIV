05442aebad0050f1d03f024f26b096182f40f200
Thais Florentin <84157862+LennyBla@users.noreply.github.com>
2023-12-04 15:47:53 -0300

    Update Movie.svelte

--- Frontends/src/routes/Movie.svelte
+++ Frontends/src/routes/Movie.svelte
@@ -5,13 +5,8 @@
   
   async function getFilmes(searchTerm) {
     try {
-      const url = searchTerm
-      ? `http://localhost:8000/filmes?title=${encodeURIComponent(searchTerm)}`
-      : `http://localhost:8000/filmes`;
-
-      const res = await fetch(url);
-      const text = await res.json();
-
+      const res = await fetch(`http://localhost:8000/filmes?title=${(searchTerm)}`);
+     const text = await res.json();
       if (res.ok) {
         return text;
       } else {
