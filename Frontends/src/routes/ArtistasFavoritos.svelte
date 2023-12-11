8661d910fe9c46f09c0a875c4a79eacd946bcf8e
Thais <84157862+LennyBla@users.noreply.github.com>
2023-12-11 18:09:22 -0300

    novo

--- Frontends/src/routes/ArtistasFavoritos.svelte
+++ Frontends/src/routes/ArtistasFavoritos.svelte
@@ -1,17 +1,12 @@
 <script>
-<<<<<<< HEAD
     // Variáveis reativas para armazenar a resposta da API, a promessa da busca e a lista de favoritos
     let resposta = "";
-=======
-    // Inicializa uma promise para obter os artistas favoritos
->>>>>>> 2eefe52247eb6082aabcf9f95b1dcb902c96cae5
     let promise = getFavoritos();
     let favoritos = [];
 
-    // Função assíncrona para obter artistas favoritos
+    // Função assíncrona para obter a lista de filmes favoritos
     async function getFavoritos() {
         try {
-<<<<<<< HEAD
             const res = await fetch(`http://localhost:8000/favoritesArtist/1`);
             console.log(res);
             const text = await res.json();
@@ -19,21 +14,11 @@
                 artistasFavoritos = text;
                 console.log(artistasFavoritos);
                 return text;
-=======
-            // Faz uma requisição assíncrona para obter os artistas favoritos do servidor
-            const res = await fetch(`http://localhost:8000/favoritesArtist/1`);
-            if (res.ok) {
-                // Se a resposta for bem-sucedida, obtém os dados em formato JSON
-                const data = await res.json();
-                favoritos = data; // Atualiza a lista de favoritos
-                return data; // Retorna os dados obtidos
->>>>>>> 2eefe52247eb6082aabcf9f95b1dcb902c96cae5
             } else {
                 throw new Error("Erro ao obter artistas favoritos");
             }
         } catch (error) {
             console.error("Erro ao obter artistas favoritos:", error);
-<<<<<<< HEAD
             throw error;
         }
     }
@@ -47,43 +32,23 @@
 
         try {
             const res = await fetch(`http://localhost:8000/deleteFavoritesArtista/1/${tmdb_id}`, {
-=======
-        }
-    }
-
-    // Função assíncrona para excluir um artista favorito
-    async function deleteFavorito(e, id) {
-        e.preventDefault();
-        try {
-            // Faz uma requisição assíncrona para excluir um artista favorito pelo ID
-            const res = await fetch(`http://localhost:8000/deleteFavoritesArtista/1/${id}`, {
->>>>>>> 2eefe52247eb6082aabcf9f95b1dcb902c96cae5
                 method: 'DELETE',
-                headers: { 'Content-Type': 'application/json' }
+                headers: {'Content-Type': 'application/json'}
             });
 
             if (res.ok) {
-<<<<<<< HEAD
                 resposta = `Artista desfavoritado`;
                 promise = getFavoritos();
-=======
-                // Se a exclusão for bem-sucedida, atualiza a lista de favoritos chamando getFavoritos()
-                await getFavoritos();
->>>>>>> 2eefe52247eb6082aabcf9f95b1dcb902c96cae5
             } else {
                 throw new Error("Erro ao desfavoritar artista");
             }
         } catch (error) {
             console.error("Erro ao desfavoritar artista:", error);
-<<<<<<< HEAD
             throw error;
-=======
->>>>>>> 2eefe52247eb6082aabcf9f95b1dcb902c96cae5
         }
     }
 </script>
 
-<<<<<<< HEAD
 <!-- Botão para carregar filmes favoritos -->
 <button on:click={handleClick}> Carregar artistas favoritos... </button>
 {#await promise}
@@ -143,23 +108,3 @@
         background-color: #ff0000;
     }
 </style>
-=======
-<!-- Botão para carregar artistas favoritos quando clicado -->
-<button on:click={getFavoritos}> Carregar artistas favoritos... </button>
-
-<!-- Bloco de código para lidar com a promise de carregamento -->
-{#await promise}
-    <p>Carregando...</p>
-{:then favoritos}
-    <!-- Exibe a lista de artistas favoritos -->
-    <h1>Meus Artistas Favoritos</h1>
-    {#each favoritos as favorito}
-        <p>Nome: {favorito.name} - Id: {favorito.id} </p>
-        <!-- Botão para desfavoritar um artista específico -->
-        <button on:click={(e) => deleteFavorito(e, favorito.id)}>Desfavoritar</button>
-    {/each}
-{:catch error}
-    <!-- Exibe uma mensagem de erro em caso de falha -->
-    <p style="color: red">Erro ao carregar: {error.message}</p>
-{/await}
->>>>>>> 2eefe52247eb6082aabcf9f95b1dcb902c96cae5
